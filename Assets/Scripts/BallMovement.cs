89973a0617dac4cfda91f0f20d7aee875df6ba43
naveen965 <39030430+naveen965@users.noreply.github.com>
2023-04-02 09:37:43 +0530

    Edit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public AnimationCurve curve;
+    public Transform[] pathPoints;
+    public bool loop = true;
+
+    private float timer;
+    private int currentPointIndex;
+
+    private void Update()
+    {
+        timer += Time.deltaTime * speed;
+
+        if (timer > 1f)
+        {
+            timer = 0f;
+
+            if (currentPointIndex == pathPoints.Length - 1)
+            {
+                if (loop)
+                {
+                    currentPointIndex = 0;
+                }
+                else
+                {
+                    enabled = false;
+                }
+            }
+            else
+            {
+                currentPointIndex++;
+            }
+        }
+
+        Vector3 startPosition = pathPoints[currentPointIndex].position;
+        Vector3 endPosition = pathPoints[(currentPointIndex + 1) % pathPoints.Length].position;
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, curve.Evaluate(timer));
+    }
+}
