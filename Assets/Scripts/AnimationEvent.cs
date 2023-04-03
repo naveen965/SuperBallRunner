c216613507feeb12a99b4163cd349773412d53ab
naveen965 <39030430+naveen965@users.noreply.github.com>
2023-04-03 21:17:14 +0530

    Added animation trigger event and edited TileManager

---
+++ Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour
+{
+    public void MyTriggerFunction(string triggerName)
+    {
+        Debug.Log("Trigger point one reached: " + triggerName);
+        // Do something in response to the trigger point being reached
+        FindObjectOfType<TileManager>().SpawnTile(Random.Range(0, FindObjectOfType<TileManager>().tilePrefabs.Length));
+    }
+
+    public void MyTriggerFunction2(string triggerName)
+    {
+        Debug.Log("Trigger point two reached: " + triggerName);
+        // Do something in response to the trigger point being reached
+        FindObjectOfType<TileManager>().DeleteTile();
+    }
+}
