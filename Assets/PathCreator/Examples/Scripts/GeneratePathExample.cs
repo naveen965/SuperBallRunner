ef5d2f82da4da0874ab9995614fa3eb7a03e723c
naveen965 <39030430+naveen965@users.noreply.github.com>
2023-04-24 20:53:48 +0530

    Edited GeneratePathExamples.cs

--- Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
+++ Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 namespace PathCreation.Examples {
     // Example of creating a path at runtime from a set of points.
@@ -6,15 +9,75 @@ namespace PathCreation.Examples {
     [RequireComponent(typeof(PathCreator))]
     public class GeneratePathExample : MonoBehaviour {
 
-        public bool closedLoop = true;
-        public Transform[] waypoints;
+        public bool closedLoop = false;
+        public bool isSpawn = false;
+        public float zSpawn = 0;
+        public float tileLength;
+        public GameObject waypoint;
+        public GameObject waypointsPerant;
 
-        void Start () {
-            if (waypoints.Length > 0) {
+        private List<Transform> waypoints = new List<Transform>();
+
+        void Start () 
+        {
+            isSpawn = true;
+
+            if (waypoints.Count > 0) {
                 // Create a new bezier path from the waypoints.
                 BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
                 GetComponent<PathCreator> ().bezierPath = bezierPath;
             }
+        }
+
+        void Update ()
+        {
+            if (isSpawn == true)
+            {
+                StartCoroutine(spawnObj());
+            }
+        }
+
+        private IEnumerator spawnObj()
+        {
+            SpawnWaypoint();
+            yield return new WaitForSeconds(3f);
+            DeleteWaypoints();
+            isSpawn = false;
+        }
+
+        public void SpawnWaypoint()
+        {
+            //GameObject spawnPosition = GameObject.FindGameObjectWithTag("SpawnPosition");
+
+            GameObject gameObject = Instantiate(waypoint, transform.forward * zSpawn, transform.rotation);
+
+            gameObject.transform.SetParent(waypointsPerant.transform);
+
+            SetWaypoints(gameObject.transform);
+
+            zSpawn += tileLength;
+        }
+
+        private void SetWaypoints(Transform waypoint)
+        {
+            waypoints.Add(waypoint);
+        }
+
+        private void DeleteWaypoints()
+        {
+            foreach (Transform waypoint in waypoints)
+            {
+                if (waypoint == waypoints[0] && waypoint.CompareTag("way"))
+                {
+                    waypoints.Remove(waypoint);
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        public List<Transform> GetWaypoints()
+        {
+            return waypoints;
         }
     }
 }
---
+++ Assets/Scripts/SphereCollid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    public class SphereCollid : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.name == "Follower")
+            {
+                FindObjectOfType<GeneratePathExample>().isSpawn = true;
+            }
+        }
+    }
+}
